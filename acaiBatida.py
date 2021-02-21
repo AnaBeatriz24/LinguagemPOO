f0445c4e4e534bdc7c9c16d55a18d576e3c96ab9
AnaBeatriz24 <78892850+AnaBeatriz24@users.noreply.github.com>
2021-02-21 20:48:14 -0300

    Create acaiBatida.py

---
+++ acaiBatida.py
@@ -0,0 +1,9 @@
+Ricardao = ['Açaí, Amendoim, mel, guaraná em pó']
+Shake = ['Açaí, Leite em Pó, Creme de Leite, Leite Condensado'] 
+ShakecFrutas = ['Açaí, Leite em Pó, Creme de Leite, Leite Condensado, Banana, Abacaxi, Uva, Mamão, Kiwi']
+
+print("___________________________________________________________")
+print("________________________Cardápio_________________________")
+print('1 - Ricardão (R$13,00)', Ricardao)
+print('2 - Shake (R$12,00)' , Shake)
+print('3 - Shake com Frutas (R$15,00)', ShakecFrutas)
