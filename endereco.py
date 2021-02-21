50e24f60bd80c7ad7987fea8e3da9d1afc69c547
AnaBeatriz24 <78892850+AnaBeatriz24@users.noreply.github.com>
2021-02-21 17:07:47 -0300

    Update endereco.py

--- endereco.py
+++ endereco.py
@@ -2,4 +2,4 @@ class Endereco:
   def __init__(self):
     self._bairro= input("Digite o nome do seu bairro: ")
     self._rua= input("Digite o nome da sua rua: ")
-    self._numero= input("Digite o número da sua casa: ")
+    self._numero= int(input("Digite o número da sua casa: "))
