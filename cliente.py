e713f492ab85148e4cd5f6b1c7cf68397d6f5d57
AnaBeatriz24 <78892850+AnaBeatriz24@users.noreply.github.com>
2021-03-03 11:05:35 -0300

    Create cliente.py

---
+++ cliente.py
@@ -0,0 +1,17 @@
+from endereco import*
+class Cliente:
+  def __init__(self):
+    print("___________________________________________________________")
+
+    self._nome= input("Digite nome: ")
+    self._telefone= input("Digite número para contato: ")
+    self._email= input("Digite seu email: ")
+    self._endereco= Endereco()
+    print("___________________________________________________________")
+
+    
+  def fazerPagamento(self):
+    print("!!Aceitamos Cartão de Crédito e Débito!!")
+    self._formapaga= input("Dinheiro, Cartão de Débito ou Crédito: ")
+    print("___________________________________________________________")
+
