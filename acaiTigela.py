cb0b444a885b52eacbdeb041fc50534dc10822f9
AnaBeatriz24 <78892850+AnaBeatriz24@users.noreply.github.com>
2021-03-03 10:57:18 -0300

    Delete acaiTigela.py

--- acaiTigela.py
+++
@@ -1,9 +0,0 @@
-Energy = ['Açaí , Banana , Leite em Pó , Creme de Leite , Leite Condensado'] 
-Ribeirinho = ['Açaí , Tapioca']
-MeleGranola = ['Açaí , Mel , Granola'] 
-AcaiKids = ['Açaí , Mm’s , Banana , Granulado , Canudo de Chocolate , Cobertura de Morango'] 
-
-print ('1 - Energy (R$16,00)', Energy)
-print ('2 - Ribeirinho (R$10,00)', Ribeirinho)
-print ('3 - Mel e Granola(R$14,00)', MeleGranola)
-print ('4 - Açaí Kids(R$18,00)', AcaiKids)
