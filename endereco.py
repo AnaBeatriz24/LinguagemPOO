b3d9efce95d724375b6e09aec6ad7385838d447d
AnaBeatriz24 <78892850+AnaBeatriz24@users.noreply.github.com>
2021-03-03 11:06:11 -0300

    Create endereco.py

---
+++ endereco.py
@@ -0,0 +1,5 @@
+class Endereco:
+  def __init__(self):
+    self._bairro= input("Digite o nome do seu bairro: ")
+    self._rua= input("Digite o nome da sua rua: ")
+    self._numero= input("Digite o número da sua casa: ")
